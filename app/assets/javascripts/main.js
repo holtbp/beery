a91c0496baf377ee80f8f2e3e381db1b6f2ad5bd
Jason Stenhouse <jason.stenhouse@gmail.com>
2014-03-25 21:25:22 -0700

    adding jem/angular-routing for routes

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -3,19 +3,34 @@
 // This line is related to our Angular app, not to our
 // HomeCtrl specifically. This is basically how we tell
 // Angular about the existence of our application.
-window.beery = angular.module('beery', ['ngRoute']).
-  config(['$routeProvider', function($routeProvider) {
-    $routeProvider.
-      when('/', {
-        templateUrl: '/assets/templates/home.html',
-        controller: 'HomeCtrl'
+var beery = angular.module('beery', ['dotjem.routing']).
+  config(['$stateProvider', function($stateProvider) {
+    $stateProvider.
+      state('home', {
+        route: '/',
+        views: {
+          main: {
+            controller: 'HomeCtrl',
+            template: 'assets/templates/home.html'
+          }
+        }
       }).
-      when('/beers', {
-        templateUrl: '/assets/templates/beers/index.html',
-        controller: 'BeerIndexCtrl'
+      state('beers', {
+        route: '/beers',
+        views: {
+          main: {
+            controller: 'BeerIndexCtrl',
+            template: 'assets/templates/beers/index.html'
+          }
+        }
       }).
-      when('/beers/:id', {
-        templateUrl: '/assets/templates/beers/show.html',
-        controller: 'BeerShowCtrl'
-      });
-    }]);
+      state('beers.show', {
+        route: '/beers/:id',
+        views: {
+          main: {
+            controller: 'BeerShowCtrl',
+            template: 'assets/templates/beers/show.html'
+          }
+        }
+      })
+  }]);
